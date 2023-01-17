386d2976be5d35e3d0db3a2843b0d66624fd3907
Millena Bomtempo <millena.bomtempo@gmail.com>
2023-01-17 14:23:08 -0300

    add avatar upload endpoint

---
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express'
+import { container } from 'tsyringe'
+
+import { UpdateUserAvatarService } from './UpdateUserAvatarService'
+
+export class UpdateUserAvatarController {
+	async handle(request: Request, response: Response): Promise<Response> {
+		const { id } = request.user
+
+		const avatar_file = request.file.filename
+
+		const updateUserAvatarService = container.resolve(UpdateUserAvatarService)
+
+		await updateUserAvatarService.execute({ user_id: id, avatar_file })
+
+		return response.status(204).send()
+	}
+}
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,25 @@
 import { Router } from 'express'
+import multer from 'multer'
 
+import uploadConfig from '../config/upload'
+import { ensureAuth } from '../middleware/ensureAuth'
 import { CreateUserController } from '../modules/accounts/useCases/createUser/CreateUserController'
+import { UpdateUserAvatarController } from '../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController'
 
 const usersRoutes = Router()
 
+const uploadAvatar = multer(uploadConfig.upload('./tmp/avatar'))
+
 const createUserController = new CreateUserController()
+const updateUserAvataeController = new UpdateUserAvatarController()
 
 usersRoutes.post('/', createUserController.handle)
+
+usersRoutes.patch(
+	'/avatar',
+	ensureAuth,
+	uploadAvatar.single('avatar'),
+	updateUserAvataeController.handle
+)
 
 export { usersRoutes }
