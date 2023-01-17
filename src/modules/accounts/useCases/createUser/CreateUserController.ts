fd36f7d4c3e3cd591a85842f739652b4dd8c8be3
Millena Bomtempo <millena.bomtempo@gmail.com>
2023-01-17 10:48:02 -0300

    add endpoint to create user

---
+++ src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express'
+import { container } from 'tsyringe'
+
+import { CreateUserService } from './CreateUserService'
+
+export class CreateUserController {
+	async handle(request: Request, response: Response): Promise<Response> {
+		const { name, email, password, driver_license } = request.body
+
+		const createUserService = container.resolve(CreateUserService)
+
+		await createUserService.execute({
+			name,
+			email,
+			password,
+			driver_license
+		})
+
+		return response.status(201).send()
+	}
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express'
 
+import { authRoutes } from './authenticate.routes'
 import { categoriesRoutes } from './categories.routes'
 import { specificationsRoutes } from './specification.routes'
+import { usersRoutes } from './users.routes'
 
 export const router = Router()
 
 router.use('/categories', categoriesRoutes)
 router.use('/specifications', specificationsRoutes)
+router.use('/users', usersRoutes)
+router.use(authRoutes)
